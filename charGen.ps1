64a62d1b71ecbca4ee4dd99163bc6b702b425d61
jgange <jgange@users.noreply.github.com>
2022-12-22 12:44:17 -0600

    Add files via upload
    Initial commit including configuration files.

---
+++ charGen.ps1
@@ -0,0 +1,40 @@
+#### INITIALIZATION ####
+
+$dataStoreLocation = "C:\Users\joega\Projects\PowerShell\crimsonReignCharacterCreator"
+$raceFile          = "races.json"
+$buildsFile        = "builds.json"
+$traitsFile        = "traits.json"
+$systemRulesFile   = "systemRules.json"
+$buildCostsFile    = "buildPointCost.json"
+
+#### METHODS ####
+
+function populateData($filePath)
+{
+    return Get-Content -Path $filePath | ConvertFrom-Json
+}
+
+function returnRecordSet($records, $propertyName, $value)
+{
+    $records | Where-Object {$_.$propertyName -eq $value}
+}
+
+#### MAIN PROGRAM ####
+
+$buildType = "Heroic"
+$race      = "Draikosi"
+
+$raceTable       = populateData ($dataStoreLocation, $raceFile -join "\") 
+$characterBuilds = populateData ($dataStoreLocation, $buildsFile -join "\") 
+$traitTable      = populateData ($dataStoreLocation, $traitsFile -join "\")
+$systemRules     = populateData ($dataStoreLocation, $systemRulesFile -join "\")
+$buildPointCosts = populateData ($dataStoreLocation, $buildCostsFile -join "\")
+
+$raceInfo  = returnRecordSet $raceTable "RaceName" $race
+$buildInfo = returnRecordSet $characterBuilds "BuildType" $buildType
+
+$raceInfo
+$buildInfo
+$traitTable
+$systemRules
+$buildPointCosts
