982051128b45f6fb4180db417815cf5f2690b17e
Joe Gange <joegange0502@gmail.com>
2022-12-23 15:33:25 -0600

    renamed file

--- charGen.ps1
+++ charGen.ps1
@@ -1,13 +1,14 @@
 #### INITIALIZATION ####
 
-$dataStoreLocation  = "C:\Users\joega\Projects\PowerShell\c_reign_pcgen"
-$raceFile           = "races.json"
-$buildsFile         = "builds.json"
-$traitsFile         = "traits.json"
-$systemRulesFile    = "systemRules.json"
-$buildCostsFile     = "buildPointCost.json"
-$attributeTableFile = "attributesTable.json"
-$characterBuild     = 'brak.json'
+$dataStoreLocation   = "C:\Users\joega\Projects\PowerShell\c_reign_pcgen"
+$raceFile            = "races.json"
+$buildsFile          = "builds.json"
+$traitsFile          = "traits.json"
+$systemRulesFile     = "systemRules.json"
+$buildCostsFile      = "buildPointCost.json"
+$attributeTableFile  = "attributesTable.json"
+$professionTiersFile = "professionTiers.json"
+$characterBuild      = "brak.json"
 
 #### METHODS ####
 
@@ -41,6 +42,7 @@ $traitTable      = populateData ($dataStoreLocation, $traitsFile -join "\")
 $systemRules     = populateData ($dataStoreLocation, $systemRulesFile -join "\")
 $buildPointCosts = populateData ($dataStoreLocation, $buildCostsFile -join "\")
 $attributeTable  = populateData ($dataStoreLocation, $attributeTableFile -join "\")
+$professionTiers = populateData ($dataStoreLocation, $professionTiersFile -join "\")
 $characterBuild  = populateData ($dataStoreLocation, $characterBuild -join "\")
 
 $raceInfo  = returnRecordSet $raceTable "RaceName" $race
@@ -53,5 +55,6 @@ $systemRules
 $buildPointCosts
 $attributeTable
 $characterBuild
+$professionTiers
 
 calculateBuildCost $characterBuild
